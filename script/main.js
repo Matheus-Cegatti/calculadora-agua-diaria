f217ed0f625d9df8e7a32bf6ba213648ada6514d
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-08-28 15:12:59 -0300

    buscando o valor(texto) das telcas da calculadora em JS

--- script/main.js
+++ script/main.js
@@ -1,3 +1,30 @@
-const botão = document.querySelector(".btn-calculadora");
-const botãoResultado = document.querySelector(".btn-confirmar");
+const botoes = document.querySelectorAll(".btn-calculadora");
+const botaoResultado = document.querySelector(".btn-confirmar");
 const tela = document.querySelector(".input-peso");
+
+class Calculadora {
+    constructor(tela) {
+        this.tela = tela;
+        this.tela = ""
+    }
+    
+}
+
+
+
+
+botoes.forEach((btn) => {
+    btn.addEventListener("click", (evento) => {
+        const valor = evento.target.innerText
+
+        // console.log(valor);
+
+        if(+valor >= 0 || valor === ".") {
+            console.log(valor);
+        }else{
+            console.log("Não é um numero ou ponto");
+        }
+    })
+})
+
+// console.log(botoes);
