85a30e595594e0ca860e59d279c652ddd4ba24b5
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-08-28 18:04:29 -0300

    pegando o retorno dos valores botão na tela

--- script/main.js
+++ script/main.js
@@ -1,17 +1,30 @@
 const botoes = document.querySelectorAll(".btn-calculadora");
 const botaoResultado = document.querySelector(".btn-confirmar");
-const tela = document.querySelector(".input-peso");
+const numeroNaTela = document.querySelector(".tela-calculadora");
 
 class Calculadora {
-    constructor(tela) {
-        this.tela = tela;
-        this.tela = ""
+    constructor(numeroNaTela) {
+        this.numeroNaTela = numeroNaTela;
+        this.numero = "";
     }
-    
+    mostraDigito(digito) {
+        // console.log(digito);
+        this.numero = digito;
+        this.atualizaTela();
+    }
+
+    atualizaTela() {
+        this.numeroNaTela.value += this.numero;
+        console.log(numeroNaTela);
+        if(numeroNaTela.value.length >= 4) {
+            // numeroNaTela.setAttribute("disabled", "disabled")
+        }
+        // console.log(numero);
+    }
 }
 
 
-
+const calc = new Calculadora(numeroNaTela);
 
 botoes.forEach((btn) => {
     btn.addEventListener("click", (evento) => {
@@ -20,7 +33,8 @@ botoes.forEach((btn) => {
         // console.log(valor);
 
         if(+valor >= 0 || valor === ".") {
-            console.log(valor);
+            // console.log(valor);
+            calc.mostraDigito(valor)
         }else{
             console.log("Não é um numero ou ponto");
         }
