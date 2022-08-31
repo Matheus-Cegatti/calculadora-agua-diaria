080843b44fd301f0086ff46059f14bbe191c9b99
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-08-30 22:17:16 -0300

    não deixando digitar mais de um "." no teclado e não deixando a operação começar com "."

--- script/main.js
+++ script/main.js
@@ -5,38 +5,68 @@ const numeroNaTela = document.querySelector(".tela-calculadora");
 const resultadoAparece = document.querySelector(".tela-resultado");
 const botaoSaibaMais = document.querySelector(".btn-saiba--mais");
 const sessaoSaibaMais = document.querySelector(".sessao-saiba--mais");
+const botaoDePonto = document.querySelector(".btn-ponto")
 
 
-botaoSaibaMais.addEventListener("click", () => {
-        sessaoSaibaMais.classList.toggle("sessao-ativada")
-    })
+
+
+
+// nao permitindo que digite mais de um ponto no teclado ou que comece digitando um ponto
+numeroNaTela.addEventListener("keydown", (evento) => {
+    const umPonto = evento.key === "."
+    
+
+    const estaVazio = evento.target.value.length === 0;
+    if (estaVazio && umPonto) {
+        evento.preventDefault()
+        return
+    }
+
+    const pontoExiste = evento.target.value.includes(".");
+    if (pontoExiste && umPonto) {
+        evento.preventDefault()
+    }
+})
 
 //digitando só numero e ponto 
 
-numeroNaTela.addEventListener("keypress", function(e){
+numeroNaTela.addEventListener("keypress", function (e) {
     const charCode = (e.charCode ? e.charCode : e.wich);
     console.log(charCode);
-    if(charCode < 45 || charCode > 58 || charCode === 47 ){
+    if (charCode < 45 || charCode > 57 || charCode === 47) {
         e.preventDefault();
     }
 })
+
+
+
+
 
 class Calculadora {
     constructor(numeroNaTela) {
         this.numeroNaTela = numeroNaTela;
         this.numero = "";
     }
+
+    //limitando "." no teclado da calculadora e não deixando começar com "." no teclado da calculadora
     mostraDigito(digito) {
 
         if (digito === "." && numeroNaTela.value.includes(".")) {
             return;
+
+
+        }
+        if(digito === "." && numeroNaTela.value.length === 0) {
+          return
         }
 
 
-        // console.log(digito);
+        console.log(digito);
         this.numero = digito;
         this.atualizaTela();
     }
+
+
 
     atualizaTela() {
 
@@ -51,12 +81,12 @@ class Calculadora {
 function operacoes() {
     botaoDeOperacao.forEach(operacao => {
         operacao.addEventListener("click", () => {
-            if(operacao.textContent === "C") {
+            if (operacao.textContent === "C") {
                 apagarCalculadora()
                 resultadoAparece.textContent = ""
-            }else if(operacao.textContent === "DEL") {
+            } else if (operacao.textContent === "DEL") {
                 deletarNumero();
-            }else if(operacao.textContent === "Confirmar") {
+            } else if (operacao.textContent === "Confirmar") {
                 confirmaResultado();
             }
         })
@@ -69,21 +99,17 @@ function apagarCalculadora() {
 }
 
 function deletarNumero() {
-    if(numeroNaTela.value.length >= 1) {
+    if (numeroNaTela.value.length >= 1) {
         let numerosDigitado = numeroNaTela.value.length;
-        numeroNaTela.value = numeroNaTela.value.substring(0, numerosDigitado-1);
+        numeroNaTela.value = numeroNaTela.value.substring(0, numerosDigitado - 1);
     }
 }
 
 function confirmaResultado() {
 
-    // if(numeroNaTela != digValido){
-    //     resultadoAparece.textContent ="Teste"
-    // }else{
-        resultadoAparece.textContent =`Você precisa tomar no minimo  ${numeroNaTela.value = Math.round(numeroNaTela.value*35)/1000} litros de agua por dia`;
-        apagarCalculadora();
-    }
-// }
+    resultadoAparece.textContent = `Você precisa tomar no minimo  ${numeroNaTela.value = Math.round(numeroNaTela.value * 35) / 1000} litros de agua por dia`;
+    apagarCalculadora();
+}
 
 
 
